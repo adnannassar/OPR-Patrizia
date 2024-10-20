c0edba7cb32f64c36359170e2ee7ad911f840966
Adnan Nassar <adnan.nassar90@gmail.com>
2024-10-20 15:21:18 +0200

    added: fixes in code

--- src/Wiederholung/Aufgaben/Aufgabe_002/MedianArray.java
+++ src/Wiederholung/Aufgaben/Aufgabe_002/MedianArray.java
@@ -4,9 +4,11 @@ import Tools.ArrayTools;
 
 public class MedianArray {
     public static void main(String[] args) {
-        int [] array = {1, 0, 4, 0, 5};
+        int[] array1 = {1, 5, 4, 3, 8};
+        int[] array2 = {1, 5, 4, 3, 2, 8};
 
 
+        System.out.println(ArrayTools.medianArray(array1));
+        System.out.println(ArrayTools.medianArray(array2));
     }
-
 }
